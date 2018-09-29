ae64e380425f6fa00b1ea7f6b266fc7a68330a7c
Max Wirt <me@maxwirt.com>
2018-09-28 21:04:08 -0700

    first draft

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,5 +1,24 @@
 WebFont.load({
     google: {
-      families: ['Droid Sans', 'Droid Serif', 'Pacifico']
+      families: ['IBM Plex Sans:400,400i,700,700i', 'Pacifico']
     }
   });
+
+$(document).ready(function(){ 
+    $('#characterLeft').text('140 characters left');
+    $('#message').keydown(function () {
+        var max = 140;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('You have reached the limit');
+            $('#characterLeft').addClass('red');
+            $('#btnSubmit').addClass('disabled');            
+        } 
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#btnSubmit').removeClass('disabled');
+            $('#characterLeft').removeClass('red');            
+        }
+    });    
+});
